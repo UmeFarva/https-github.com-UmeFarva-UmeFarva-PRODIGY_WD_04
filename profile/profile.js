dc48dd339a912db87ebfa7aff2b8ba39a81bc907
UmeFarva <119131203+UmeFarva@users.noreply.github.com>
2024-03-05 22:26:07 +0500

    Add files via upload

---
+++ profile/profile.js
@@ -0,0 +1,7 @@
+function openLinkedIn() {
+    window.open('https://linkedin.com/your-profile', '_blank');
+}
+
+function openGitHub() {
+    window.open('https://github.com/your-username', '_blank');
+}
